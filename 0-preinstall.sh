a5782d230274aca0efe151d9195bd550f7c57cf5
OlaYZen <94807067+OlaYZen@users.noreply.github.com>
2021-12-11 23:36:16 +0100

    Update 0-preinstall.sh

--- 0-preinstall.sh
+++ 0-preinstall.sh
@@ -43,7 +43,7 @@ lsblk
 echo "Please enter disk to work on: (example /dev/sda)"
 read DISK
 echo "THIS WILL FORMAT AND DELETE ALL DATA ON THE DISK"
-read -p "are you sure you want to continue (Y/N):" formatdisk
+read -p "are you sure you want to continue (y/N):" formatdisk
 case $formatdisk in
 
 y|Y|yes|Yes|YES)
